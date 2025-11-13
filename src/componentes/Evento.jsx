b1cc2a36d0b91db2412d7857fa2e75909d1f0c06
Joana <joana.favaretto@edu.sc.senai.br>
2025-11-13 14:00:31 -0300

    Adicionado CSS dividindo os arquivos

--- src/componentes/Evento.jsx
+++ src/componentes/Evento.jsx
@@ -1,16 +1,18 @@
 // Esta linha define uma função chamada "Evento"
 // Ela recebe um parâmetro chamado "props", que contém os dados passados para o componente
-function Evento(nome,data,local) {
+import "./evento.css";
+import React from "react";
+
+function Evento(props) {
     return (
       <div className="evento">
-        <h3>{nome}</h3>{" "}
-       <p>nome: {nome}</p>{" "}
-       <p>data: {data}</p>
-       <p>local: {local}</p>
+      <h3>{props.nome}</h3>{" "}
+      <p> Data: {props.data}{" "}</p>
+       <p> Local: {props.local}{" "}</p>
       </div>
     );
   
       }
-        export default Evento;
+   export default Evento;
 
-  
+          
